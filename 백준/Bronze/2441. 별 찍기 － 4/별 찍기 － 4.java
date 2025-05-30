7b5023e9b71f03288f842682bd40091b9ac7ceca
Donghyuk Yoo <39060720+eastdh@users.noreply.github.com>
2025-05-30 17:10:18 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 352 ms, Memory: 18444 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int row=1; row<=n; row++) {
+            for (int space=1; space<=row-1; space++) {
+                System.out.print(" ");
+            }
+            for (int star=1; star<=n-(row-1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
