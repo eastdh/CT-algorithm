1658ecbc7c7ca3bd14b25ab00992ba9a0c9cddf8
Donghyuk Yoo <eastdh@gmail.com>
2026-07-24 15:06:21 +0900

    [level 0] Title: x 사이의 개수, Time: 4.41 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -2,15 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-  
-
   public int[] solution(String myString) {
-    List<Integer> answer = new ArrayList<>();
-    String[] splited = myString.split("x", -1);
-    for (String s : splited) {
-      answer.add(s.length());
+    String[] splitted = myString.split("x", -1);
+    int[] answer = new int[splitted.length];
+    for (int i = 0; i < splitted.length; i++) {
+      answer[i] = splitted[i].length();
     }
 
-    return answer.stream().mapToInt(Integer::intValue).toArray();
+    return answer;
   }
 }
