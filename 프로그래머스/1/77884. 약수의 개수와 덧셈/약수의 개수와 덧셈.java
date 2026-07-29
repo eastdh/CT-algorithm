81c331b736d7f16d0bac39475b03fff87a88c8a2
Donghyuk Yoo <eastdh@gmail.com>
2026-07-29 17:41:41 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,15 @@
+class Solution {
+  
+
+  public int solution(int left, int right) {
+    int answer = 0;
+    for (int i = left; i <= right; i++) {
+      if (Math.pow((int) Math.sqrt(i), 2) == i) {
+        answer -= i;
+      } else {
+        answer += i;
+      }
+    }
+    return answer;
+  }
+}
