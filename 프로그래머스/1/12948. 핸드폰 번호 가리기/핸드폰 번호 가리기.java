e166412805007e6241fad24a84a5c075485585c8
Donghyuk Yoo <eastdh@gmail.com>
2026-07-29 17:17:07 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.86 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,8 @@
+class Solution {
+  
+
+  public String solution(String phone_number) {
+    int len = phone_number.length();
+    return "*".repeat(len - 4) + phone_number.substring(len - 4, len);
+  }
+}
